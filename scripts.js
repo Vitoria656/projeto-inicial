6584c31c3bb3311999f671741ac0b5eafdcadb36
vitoria656 <vjordana101@gmail.com>
2024-03-19 08:24:01 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -2,7 +2,20 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
 function alertar(event){
-    alert("Você clicou no botão!!!! " + nome.value);
+   //alert("Voce clicou no botão!!!! " nome.value);
+
+     saida.innerText = "Nome: " + nome.value +
+                     "\n Email: " + email.value +
+                     "\n Telefone: " + telefone.value +
+                     "\n CEP: " + cep.value +
+                     "\n logradouro: " + logardouro.value +
+
+                    
+
 }
  
