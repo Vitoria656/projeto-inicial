ca3b450ae6bfa64a5cc7bde092a714f51e774fd8
vitoria656 <vjordana101@gmail.com>
2024-03-19 08:48:18 -0300

    f

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro"); 
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+
 
 var saida = document.getElementById("saida-de-dados");
 
-var email = document.getElementById("email");
 
 function alertar(event){
    //alert("Voce clicou no botão!!!! " nome.value);
@@ -13,7 +22,13 @@ function alertar(event){
                      "\n Email: " + email.value +
                      "\n Telefone: " + telefone.value +
                      "\n CEP: " + cep.value +
-                     "\n logradouro: " + logardouro.value +
+                     "\n logradouro: " + logradouro.value +
+                     "\n numero: " + numero.value +
+                     "\n complemento: " + complemento.value +
+                     "\n bairro: " + bairro.value +
+                     "\n cidade: " + cidade.value +
+                     "\n estado: " + estado.value;
+
 
                     
 
